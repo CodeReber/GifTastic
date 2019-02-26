ca29932b8559c9d9243fdf29ade51aca611cbd0f
codereber <jamesmpyle@gmail.com>
2019-02-26 07:30:10 -0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert("test");
+//var to track
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
+
+//functions
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function (response){
+        console.log(queryURL);
+        console.log(response);
+
+    });
+
+
+
+
+
+
+
+//main process
+
+//events
+$("#rock").click(function () { 
+   alert("button press");
+});
+$("#input").click(function () {
+    var inputBox = $("#input").val().trim();
+    console.log(inputBox); 
+});
